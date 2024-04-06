a54523dc71f020dda0e7169f81ffa35d7f8f7b7d
vishal Biradar <biradarvishal756@gmail.com>
2024-04-06 23:22:28 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-
+import React from "react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import './App.css';
+import Companines from "./components/companines/Companines";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+import Contact from "./components/contact/Contact";
+import GetStared from "./components/GetStarted/GetStared";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+    <div>
+    <div className="white-gradient"></div>
+    <Header/>
+    <Hero/>
+    </div>
+    <Companines/>
+    <Residencies/>
+    <Value/>
+    <Contact/>
+    <GetStared/>
+    <Footer/>
+    </div>  
   );
 }
 
